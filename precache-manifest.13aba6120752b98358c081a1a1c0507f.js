4db8065943e2ffd356231c2ec471077c70776fa0
chotardlucien <chotardlucien@gmail.com>
2020-07-05 16:02:06 +0200

    Updates

---
+++ precache-manifest.13aba6120752b98358c081a1a1c0507f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc2230c70baa7bc3960e4cde7e38a40",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6dcf628ec05fe78a56e7",
+    "url": "/portfolio/static/css/main.62151a08.chunk.css"
+  },
+  {
+    "revision": "a6defc179106908572e9",
+    "url": "/portfolio/static/js/2.ab7ebf7e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.ab7ebf7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dcf628ec05fe78a56e7",
+    "url": "/portfolio/static/js/main.fcf81108.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "d5b402a76c1f3d5037eae506f8af27e1",
+    "url": "/portfolio/static/media/AvenirLTProBlack.d5b402a7.otf"
+  },
+  {
+    "revision": "e899c1f1fe23835bfc27bb898ff94d8b",
+    "url": "/portfolio/static/media/AvenirLTProBook.e899c1f1.otf"
+  },
+  {
+    "revision": "833739a8ba3238630b2376b30a521bde",
+    "url": "/portfolio/static/media/AvenirLTProRoman.833739a8.otf"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/portfolio/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "fb7e3a294cb07a54605a8bb27f0cd528",
+    "url": "/portfolio/static/media/OpenSans-ExtraBold.fb7e3a29.ttf"
+  },
+  {
+    "revision": "bbd611432fe5bface42de6e9dd299d3a",
+    "url": "/portfolio/static/media/arrow_left.bbd61143.svg"
+  },
+  {
+    "revision": "e1b753df8ceb4ee600061625a88724e4",
+    "url": "/portfolio/static/media/arrow_right.e1b753df.svg"
+  },
+  {
+    "revision": "b29d9768b70cf318bebad064fc1f36f0",
+    "url": "/portfolio/static/media/logo.b29d9768.svg"
+  }
+]);
