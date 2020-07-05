eefc89ed9e13619d12406233935b515cccfa371a
chotardlucien <chotardlucien@gmail.com>
2020-07-05 23:37:01 +0200

    Revert "test"
    This reverts commit 08f7bcb4a0b462399535107e7b2dea3338abf4bc.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -17,7 +17,7 @@ function App() {
     <BrowserRouter>
       <Wrapper>
         <Header></Header>
-        <Redirect exact from="/portfolio/" to="/portfolio/projects" />
+        <Redirect exact from="/portfolio/" to="projects" />
         <Switch>
         <Route path="/portfolio/about" component={About}></Route>
         <Route path="/portfolio/projects" component={Projects}></Route>
